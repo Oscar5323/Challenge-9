115d434716d9d2b089acd006a6af17483f0189d3
Oscar5323 <jr.mercado2001@gmail.com>
2023-08-03 22:32:46 -0500

    bug testing

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require("express");
-const html_route = require('./routes/html.routes');
-const api_route = require('./routes/api-routes');
-const PORT = 3001;
+const fs = require("fs");
+const path = require('path');
 const app = express();
+const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(html_route)
-app.use(api_route)
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/public/index.html'))
+});
+
+app.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/public/notes.html'))
+});
+
+app.get('/api/notes', (req,res)=>{
+    const dbjson =JSON.parse(fs.readFileSync("db/db.json"));
+    res.json(dbjson);
+});
+
+app.post('/api/notes', (req,res)=>{
+    const dbjson = JSON.parse(fs.readFileSync('db/db.json'));
+    const newnotes = {
+        title: req.body.title,
+        text: req.body.text,
+    };
+    dbjson.push(newnotes);
+    fs.writeFileSync('db/db.json', JSON.stringify(dbjson));
+    res.json(dbjson);
+});
+
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
